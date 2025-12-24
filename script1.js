590d4b5f2a11eb17b7686fedb7ed54e3c548b97d
sgc <3563824183@qq.com>
2025-12-24 22:12:12 +0800

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded',function(){
     const selectorItems = document.querySelectorAll('.selector-item')
-    const selectorItems1 = document.querySelectorAll('.selector-item1')
     const sidebars = document.querySelectorAll('.sidebar')
     const target = document.getElementById('container')
     
@@ -23,15 +22,9 @@ document.addEventListener('DOMContentLoaded',function(){
         })
     })
 
-    selectorItems1.forEach(i => {
-        i.addEventListener('click',function(){
-            selectorItems1.forEach(i => {i.classList.remove('active')})
-            this.classList.add('active');
-        })
-    })
-
     document.getElementById('button').addEventListener('click',function(){
         target.scrollIntoView({ behavior: 'smooth', block:'start'})
     })
 
 })
+
